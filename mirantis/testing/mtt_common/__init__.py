23549dd934e2b695e62c0bee2a7dd1ecc5955227
James Nesbitt <jnesbitt@mirantis.com>
2021-01-11 12:10:18 -0500

    mtt_common: some boostrap source cleanup

--- mirantis/testing/mtt_common/__init__.py
+++ mirantis/testing/mtt_common/__init__.py
@@ -3,4 +3,7 @@ import os
 
 def mtt_common_bootstrap(config):
     """ bootstrap the passed toolbox config for mtt common functionality """
-    config.sources.add_filepath_source(os.path.dirname(os.path.realpath(__file__)), "mtt_common", 60)
+    mtt_common_path = os.path.dirname(os.path.realpath(__file__))
+    mtt_common_config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "config")
+    config.sources.add_filepath_source(mtt_common_config_path, "mtt_common", 60)
+    config.sources.add_filepath_source(mtt_common_path, "mtt_common_root", 60)
