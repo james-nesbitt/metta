50e15369735f262b45abd31b6e888f034b7249b9
James Nesbitt <jnesbitt@mirantis.com>
2021-07-22 20:06:29 -0400

    ansible plugins accept vars and envs
    - workload and provisioner plugins accept config for vars and envs
    - client plugins accept inline vars, vars from files, and inline envs
    - smoke test uses vars and envs.
    
    Signed-off-by: James Nesbitt <jnesbitt@mirantis.com>

--- mirantis/testing/metta_ansible/ansiblecli_workload.py
+++ mirantis/testing/metta_ansible/ansiblecli_workload.py
@@ -40,12 +40,24 @@ ANSIBLE_WORKLOAD_CONFIG_LABEL = "ansible"
 
 ANSIBLE_WORKLOAD_CONFIG_ARGS_KEY = "args"
 """Configures key for loading args."""
-ANSIBLE_WORKLOAD_CONFIG_ENVS_KEY = "env"
+ANSIBLE_WORKLOAD_CONFIG_ENVS_KEY = "envs"
 """Configerus key for loading env variables to be used for clients."""
 ANSIBLE_WORKLOAD_CONFIG_PLAYBOOK_KEY = "playbook.contents"
-"""Configerus key for loading playbook groups/content."""
+""" config key for the ansible playbook content """
 ANSIBLE_WORKLOAD_CONFIG_PLAYBOOK_PATH_KEY = "playbook.path"
-"""Configerus key for finding what path to use for writing the playbook yaml."""
+""" config key for the ansible playbook path to write to."""
+ANSIBLE_WORKLOAD_CONFIG_PLAYBOOK_VARS_KEY = "vars.values"
+""" config key for the ansible playbook vars."""
+ANSIBLE_WORKLOAD_CONFIG_PLAYBOOK_VARS_PATH_KEY = "vars.path"
+""" config key for the ansible playbook vars path to write to."""
+ANSIBLE_WORKLOAD_CONFIG_ANSIBLECFG_KEY = "ansiblecfg.contents"
+""" config key for the ansible cfg contents which will be written to file """
+ANSIBLE_WORKLOAD_CONFIG_ANSIBLECFG_PATH_KEY = "ansiblecfg.path"
+""" config key for the ansible cfg file path to write to."""
+ANSIBLE_WORKLOAD_CONFIG_INVENTORY_KEY = "inventory.contents"
+""" config key for the ansible  inventory, which will be passed to ansible """
+ANSIBLE_WORKLOAD_CONFIG_INVENTORY_PATH_KEY = "inventory.path"
+""" config key for the ansible inventory file path to write to."""
 
 
 class AnsibleCliCoreWorkloadPlugin:
@@ -128,10 +140,32 @@ class AnsibleCliPlaybookWorkloadPlugin:
 
     def info(self, deep: bool = False):
         """get info about a provisioner plugin"""
+        plugin_config = self._environment.config.load(self._config_label)
+        """Loaded configerus config for the plugin. Ready for .get()."""
+
         return {
             "config": {
                 "label": self._config_label,
                 "base": self._config_base,
+            },
+            "plugin": {
+                "playbook_contents": plugin_config.get(
+                    [self._config_base, ANSIBLE_WORKLOAD_CONFIG_PLAYBOOK_KEY], default={}
+                ),
+                "playbook_path": plugin_config.get(
+                    [self._config_base, ANSIBLE_WORKLOAD_CONFIG_PLAYBOOK_PATH_KEY],
+                    default="",
+                ),
+                "envs": plugin_config.get(
+                    [self._config_base, ANSIBLE_WORKLOAD_CONFIG_ENVS_KEY], default={}
+                ),
+                "vars_values": plugin_config.get(
+                    [self._config_base, ANSIBLE_WORKLOAD_CONFIG_PLAYBOOK_VARS_KEY], default={}
+                ),
+                "vars_path": plugin_config.get(
+                    [self._config_base, ANSIBLE_WORKLOAD_CONFIG_PLAYBOOK_VARS_PATH_KEY],
+                    default="",
+                ),
             },
             "client-plugin": self._ansibleplaybook_client.info(deep=deep)
             if self._ansibleplaybook_client
@@ -143,9 +177,42 @@ class AnsibleCliPlaybookWorkloadPlugin:
         if fixtures is None:
             fixtures = self._environment.fixtures
 
-        self._ansibleplaybook_client = fixtures.get_plugin(
+        self._ansibleplaybook_client: AnsiblePlaybookClientPlugin = fixtures.get_plugin(
             plugin_id=METTA_ANSIBLE_ANSIBLECLI_PLAYBOOKCLIENT_PLUGIN_ID
         )
+
+        plugin_config = self._environment.config.load(self._config_label)
+        """Loaded configerus config for the plugin. Ready for .get()."""
+
+        playbook_contents: str = plugin_config.get(
+            [self._config_base, ANSIBLE_WORKLOAD_CONFIG_PLAYBOOK_KEY], default={}
+        )
+        playbook_path: str = plugin_config.get(
+            [self._config_base, ANSIBLE_WORKLOAD_CONFIG_PLAYBOOK_PATH_KEY],
+            default="",
+        )
+        vars_values: str = plugin_config.get(
+            [self._config_base, ANSIBLE_WORKLOAD_CONFIG_PLAYBOOK_VARS_KEY], default={}
+        )
+        vars_path: str = plugin_config.get(
+            [self._config_base, ANSIBLE_WORKLOAD_CONFIG_PLAYBOOK_VARS_PATH_KEY],
+            default="",
+        )
+
+        if playbook_contents:
+            os.makedirs(os.path.dirname(os.path.realpath(playbook_path)), exist_ok=True)
+            with open(playbook_path, "w") as playbook_fileobject:
+                yaml.safe_dump(playbook_contents, playbook_fileobject)
+        else:
+            if playbook_path and os.path.exists(playbook_path):
+                os.remove(playbook_path)
+        if vars_values:
+            os.makedirs(os.path.dirname(os.path.realpath(vars_path)), exist_ok=True)
+            with open(vars_path, "w") as vars_fileobject:
+                yaml.safe_dump(vars_values, vars_fileobject)
+        else:
+            if vars_path and os.path.exists(vars_path):
+                os.remove(vars_path)
 
     def apply(self):
         """Apply the workload to the environment."""
@@ -162,6 +229,13 @@ class AnsibleCliPlaybookWorkloadPlugin:
         envs: Dict[str, str] = plugin_config.get(
             [self._config_base, ANSIBLE_WORKLOAD_CONFIG_ENVS_KEY], default={}
         )
+        vars_values: str = plugin_config.get(
+            [self._config_base, ANSIBLE_WORKLOAD_CONFIG_PLAYBOOK_VARS_KEY], default={}
+        )
+        vars_path: str = plugin_config.get(
+            [self._config_base, ANSIBLE_WORKLOAD_CONFIG_PLAYBOOK_VARS_PATH_KEY],
+            default="",
+        )
 
         if playbook_contents:
             os.makedirs(os.path.dirname(os.path.realpath(playbook_path)), exist_ok=True)
@@ -170,5 +244,14 @@ class AnsibleCliPlaybookWorkloadPlugin:
         else:
             if playbook_path and os.path.exists(playbook_path):
                 os.remove(playbook_path)
+        if vars_values:
+            os.makedirs(os.path.dirname(os.path.realpath(vars_path)), exist_ok=True)
+            with open(vars_path, "w") as vars_fileobject:
+                yaml.safe_dump(vars_values, vars_fileobject)
+        else:
+            if vars_path and os.path.exists(vars_path):
+                os.remove(vars_path)
 
-        return self._ansibleplaybook_client.run_file(playbooksyml_path=playbook_path, envs=envs)
+        return self._ansibleplaybook_client.run_file(
+            playbooksyml_path=playbook_path, extravars_path=vars_path, envs=envs
+        )
