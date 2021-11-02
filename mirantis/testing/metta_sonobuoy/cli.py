407693930fab4c871b07e02d4542ec11d6b52273
James Nesbitt <jnesbitt@mirantis.com>
2021-11-02 09:47:47 -0400

    sonobuoy refactor
    Signed-off-by: James Nesbitt <jnesbitt@mirantis.com>

--- mirantis/testing/metta_sonobuoy/cli.py
+++ mirantis/testing/metta_sonobuoy/cli.py
@@ -13,7 +13,8 @@ from mirantis.testing.metta.environment import Environment
 from mirantis.testing.metta.fixture import Fixture
 from mirantis.testing.metta_cli.base import CliBase, cli_output
 
-from .client import METTA_SONOBUOY_CLIENT_PLUGIN_ID
+from .client import METTA_SONOBUOY_CLIENT_PLUGIN_ID, SonobuoyClientPlugin
+from .workload import METTA_SONOBUOY_WORKLOAD_PLUGIN_ID
 from .results import Status
 
 logger = logging.getLogger("metta.cli.sonobuoy")
@@ -50,6 +51,15 @@ class SonobuoyClientGroup:
         """Inject Environment into command group."""
         self._environment: Environment = environment
 
+        if (
+            self._environment.fixtures().get(
+                plugin_id=METTA_SONOBUOY_WORKLOAD_PLUGIN_ID,
+                exception_if_missing=False,
+            )
+            is not None
+        ):
+            self.workload = SonobuoyWorkloadGroup(self._environment)
+
     def _select_fixture(self, instance_id: str = "") -> Fixture:
         """Pick a matching client plugin."""
         try:
@@ -66,12 +76,6 @@ class SonobuoyClientGroup:
 
         except KeyError as err:
             raise ValueError("No usable sonobuoy client was found.") from err
-
-    def _prepared_fixture(self, instance_id: str = "") -> Fixture:
-        """Pick a matching client plugin and prepare it."""
-        sonobuoy_fixture: Fixture = self._select_fixture(instance_id=instance_id)
-        sonobuoy_fixture.plugin.prepare()
-        return sonobuoy_fixture
 
     def info(self, instance_id: str = "", deep: bool = False):
         """Get info about a sonobuoy plugin."""
@@ -143,6 +147,12 @@ class SonobuoyClientGroup:
         client_plugin = self._select_fixture(instance_id=instance_id).plugin
         client_plugin.logs(follow=follow)
 
+    def results(self, instance_id: str = ""):
+        """Retrieve sonobuoy results."""
+        client_plugin = self._select_fixture(instance_id=instance_id).plugin
+        results: str = client_plugin.results()
+        return results
+
     def retrieve(self, instance_id: str = ""):
         """Retrieve the results from the sonobuoy workload instance."""
         client_plugin = self._select_fixture(instance_id=instance_id).plugin
@@ -158,12 +168,6 @@ class SonobuoyClientGroup:
         """Retrieve the sonobuoy version string."""
         client_plugin = self._select_fixture(instance_id=instance_id).plugin
         return cli_output(client_plugin.version())
-        # try:
-        #     return cli_output(client_plugin.version())
-        # # broad catch to allow message formatting for cli output
-        # # pylint: disable=broad-except
-        # except Exception as err:
-        #     logger.error("Version Retrieve failed: %s", err)
 
     def health(self, instance_id: str = ""):
         """Retrieve the results from the sonobuoy workload instance."""
@@ -176,3 +180,78 @@ class SonobuoyClientGroup:
                 "messages": list(health.messages()),
             }
         )
+
+    def create_k8s_crb(self, instance_id: str = ""):
+        """Create the cluster role binding that sonobuoy needs."""
+        client_plugin = self._select_fixture(instance_id=instance_id).plugin
+        return cli_output(client_plugin._sonobuoy.create_k8s_crb())
+
+    def delete_k8s_crb(self, instance_id: str = ""):
+        """Remove the cluster role binding that we created."""
+        client_plugin = self._select_fixture(instance_id=instance_id).plugin
+        return cli_output(client_plugin._sonobuoy.delete_k8s_crb())
+
+class SonobuoyWorkloadGroup:
+    """Sonobuoy workload commands."""
+
+    def __init__(self, environment: Environment):
+        """Inject Environment into command group."""
+        self._environment: Environment = environment
+
+    def _select_fixture(self, instance_id: str = "") -> Fixture:
+        """Pick a matching workload plugin."""
+        try:
+            if instance_id:
+                return self._environment.fixtures().get(
+                    plugin_id=METTA_SONOBUOY_WORKLOAD_PLUGIN_ID,
+                    instance_id=instance_id,
+                )
+
+            # Get the highest priority provisioner
+            return self._environment.fixtures().get(
+                plugin_id=METTA_SONOBUOY_WORKLOAD_PLUGIN_ID,
+            )
+
+        except KeyError as err:
+            raise ValueError("No usable sonobuoy client was found.") from err
+
+    def _prepared_fixture(self, instance_id: str = "") -> Fixture:
+        """Pick a matching workload plugin and prepare it."""
+        sonobuoy_fixture: Fixture = self._select_fixture(instance_id=instance_id)
+        sonobuoy_fixture.plugin.prepare()
+        return sonobuoy_fixture
+
+    def _client_plugin(self, instance_id: str = "") -> SonobuoyClientPlugin:
+        """Get the workload's client plugin."""
+        return self._prepared_fixture(instance_id=instance_id).plugin.get_client_plugin()
+
+    def info(self, instance_id: str = "", deep: bool = False):
+        """Get info about a sonobuoy plugin."""
+        fixture = self._select_fixture(instance_id=instance_id)
+        return cli_output(fixture.info(deep=deep))
+
+    def apply(self, instance_id: str = "", wait: bool = False):
+        """Run sonobuoy."""
+        workload_plugin = self._select_fixture(instance_id=instance_id).plugin
+        workload_plugin.apply(wait=wait)
+
+    def destroy(self, instance_id: str = "", wait: bool = False):
+        """Remove all sonobuoy infrastructure."""
+        workload_plugin = self._select_fixture(instance_id=instance_id).plugin
+        workload_plugin.destroy(wait=wait)
+
+    def logs(self, instance_id: str = "", follow: bool = False):
+        """Retrieve sonobuoy logs."""
+        client_plugin = self._client_plugin(instance_id=instance_id)
+        client_plugin.logs(follow=follow)
+
+    def retrieve(self, instance_id: str = ""):
+        """Retrieve the results from the sonobuoy workload instance."""
+        client_plugin = self._client_plugin(instance_id=instance_id)
+
+        try:
+            client_plugin.retrieve()
+        # broad catch to allow message formatting for cli output
+        # pylint: disable=broad-except
+        except Exception as err:
+            logger.error("Retrieve failed: %s", err)
